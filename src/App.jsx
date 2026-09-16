e751f250f0e32cc5b1764ef7b7875fa06c0cf001
Steven <avilasteven992@gmail.com>
2026-09-15 21:13:00 -0500

    first commit of band management tool for react practice

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import GigList from './components/GigList'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Band Management Tool</h1>
+      <GigList />
+    </div>
+  )
+}
+
+export default App
